ec9a77fcbf4ee4200c3ef370b70a91976b635f27
DAN <mozhayen@bk.ru>
2023-12-15 13:18:13 +0300

    bug fixes

--- logic/searchLogic.js
+++ logic/searchLogic.js
@@ -1,21 +1,20 @@
 $(document).on("input",function(ev){
-searchForCities(document.getElementById("inputBox").value,response =>{
+    var userInput =document.getElementById("inputBox").value;
+searchForCities(userInput,response =>{
+    const box = document.getElementById('theHintMenu');
+    box.innerHTML = '';
     for (let i =0;i<=response.length;i++){
         let city = response[i];
         console.log(city)
-
-
         $("<div/>", {
             id: 'link_html_edit',
             addClass: "hints",
-            text: city.name,
+            text: city.name+"-"+city.region,
+
             click: function(){
-                window.open('http://localhost:63342/Weather%20forecast/weather%20information/inner.html?_ijt=jbmmftuak4656uspd96p2en6tt&_ij_reload=RELOAD_ON_SAVE')
+                window.open('http://localhost:63342/Weather%20forecast/weather%20information/inner.html?_ijt=jbmmftuak4656uspd96p2en6tt&_ij_reload=RELOAD_ON_SAVE/'+userInput)
             }
         }).appendTo(theHintMenu);
-
     }
-
 });
-
 });
